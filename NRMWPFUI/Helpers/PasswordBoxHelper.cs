19de2463df82b6556c8564452a5fff62b49c95fe
Novinyo AMEGADJE <novinyolearn@gmail.com>
2019-03-12 15:48:11 +0000

    Added Login Form and MVVM logic for password field

---
+++ NRMWPFUI/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NRMWPFUI.Helpers
+{
+    public class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword",
+                typeof(string),
+                typeof(PasswordBoxHelper),
+                new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+
+            if (d is PasswordBox box)
+            {
+                box.PasswordChanged -= PasswordChanged;
+                box.PasswordChanged += PasswordChanged;
+            }
+
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            if (string.Equals(value, GetBoundPassword(d)))
+                return; // prevent infinite recursion
+
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            if (!(d is PasswordBox box))
+                return;
+
+            box.Password = GetBoundPassword(d);
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox password = sender as PasswordBox;
+
+            SetBoundPassword(password, password.Password);
+
+            // Set cursor past the last character in the password box
+            password.GetType().GetMethod("Select", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Invoke(password, new object[] { password.Password.Length, 0});
+        }
+
+    }
+}
